1bbfbaaf8af3da702cad7135571283c736a8f8ff
igum-natasha <igum.natasha@gmail.com>
2023-01-18 02:12:08 +0300

    Add test version of bar and pie charts

--- app/src/main/java/com/example/budgetup/HomeActivity.java
+++ app/src/main/java/com/example/budgetup/HomeActivity.java
@@ -1,5 +1,7 @@
 package com.example.budgetup;
 
+import static com.github.mikephil.charting.utils.ColorTemplate.*;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,7 +9,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -16,8 +26,8 @@ public class HomeActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_home);
 
+    initPieChart();
     BottomNavigationView nav_view = findViewById(R.id.navigationView);
-
     nav_view.setSelectedItemId(R.id.home);
     nav_view.setOnNavigationItemSelectedListener(
         new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -39,5 +49,40 @@ public class HomeActivity extends AppCompatActivity {
             return false;
           }
         });
+  }
+
+  private void initPieChart() {
+    PieChart pieChart = findViewById(R.id.pieChart);
+    ArrayList<PieEntry> expenses = new ArrayList<>();
+    expenses.add(new PieEntry(508, "Food"));
+    expenses.add(new PieEntry(306, "Clothes"));
+    expenses.add(new PieEntry(150, "Car"));
+    expenses.add(new PieEntry(1400, "Gifts"));
+    expenses.add(new PieEntry(100, "House"));
+    expenses.add(new PieEntry(350, "Transport"));
+    int[] colors = {
+            getResources().getColor(R.color.menu_1),
+            getResources().getColor(R.color.menu_2),
+            getResources().getColor(R.color.menu_3),
+            getResources().getColor(R.color.menu_4),
+            getResources().getColor(R.color.menu_5),
+            getResources().getColor(R.color.menu_6)};
+    PieDataSet pieDataSet = new PieDataSet(expenses, "Expenses");
+    pieDataSet.setColors(colors);
+    pieDataSet.setDrawValues(false);
+
+    PieData pieData = new PieData(pieDataSet);
+    Legend l = pieChart.getLegend();
+    l.setEnabled(false);
+    pieChart.setData(pieData);
+//    pieChart.setDrawEntryLabels(false);
+    pieChart.setEntryLabelColor(getResources().getColor(R.color.base_600));
+    pieChart.getDescription().setEnabled(false);
+    pieChart.setCenterText("RUB 2814");
+    pieChart.setCenterTextSize(18f);
+    pieChart.setCenterTextColor(getResources().getColor(R.color.error_800));
+    pieChart.animate();
+
+
   }
 }
--- app/src/main/java/com/example/budgetup/JournalFragment.java
+++ app/src/main/java/com/example/budgetup/JournalFragment.java
@@ -8,12 +8,80 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.BarLineChartBase;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class JournalFragment extends Fragment {
 
+  private View view;
   @Override
   public View onCreateView(
       LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_journal, container, false);
+    view = inflater.inflate(R.layout.fragment_journal, container, false);
+    initBarChart();
+    return view;
+  }
+
+  private void initBarChart() {
+    BarChart barChart = (BarChart) view.findViewById(R.id.barChart);
+    ArrayList<BarEntry> expenses = new ArrayList<>();
+    expenses.add(new BarEntry(0, 508));
+    expenses.add(new BarEntry(1, 306));
+    expenses.add(new BarEntry(2, 150));
+    expenses.add(new BarEntry(3, 1400));
+    expenses.add(new BarEntry(4, 100));
+    expenses.add(new BarEntry(5, 350));
+    ArrayList<String> xAxisLabel = new ArrayList<>();
+    xAxisLabel.add("Food");
+    xAxisLabel.add("Clothes");
+    xAxisLabel.add("Car");
+    xAxisLabel.add("Gifts");
+    xAxisLabel.add("House");
+    xAxisLabel.add("Transport");
+
+    XAxis xAxis = barChart.getXAxis();
+    xAxis.setValueFormatter(new IndexAxisValueFormatter(xAxisLabel));
+
+    BarDataSet barDataSet = new BarDataSet(expenses, "Expenses");
+    int[] colors = {
+            getResources().getColor(R.color.menu_1),
+            getResources().getColor(R.color.menu_2),
+            getResources().getColor(R.color.menu_3),
+            getResources().getColor(R.color.menu_4),
+            getResources().getColor(R.color.menu_5),
+            getResources().getColor(R.color.menu_6)};
+    barDataSet.setColors(colors);
+    barDataSet.setDrawValues(false);
+
+    BarData barData = new BarData(barDataSet);
+    Legend l = barChart.getLegend();
+    l.setEnabled(false);
+    xAxis.setPosition ( XAxis.XAxisPosition.BOTTOM);
+    xAxis.setDrawGridLines(false);
+    xAxis.setDrawAxisLine(false);
+    xAxis.setGranularity(1f);
+    xAxis.setLabelCount(xAxisLabel.size());
+    barChart.setFitBars(true);
+    barChart.setData(barData);
+    barChart.getDescription().setEnabled(false);
+    barChart.animateY(1500);
   }
 }
