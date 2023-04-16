f5145920d75c45b7b0604e5e0a2e2027591507ca
igum-natasha <igum.natasha@gmail.com>
2023-04-16 21:36:17 +0300

    Add notification one per week

---
+++ app/src/main/java/com/example/budgetup/Receiver.java
@@ -0,0 +1,35 @@
+package com.example.budgetup;
+
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class Receiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        showNotification(context);
+    }
+
+    public void showNotification(Context context) {
+        Intent intent = new Intent(context, HomeActivity.class);
+        int reqCode = 0;
+        @SuppressLint("UnspecifiedImmutableFlag")
+        PendingIntent pi = PendingIntent.getActivity(context, reqCode, intent, 0);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, "notifyBudgetUp")
+                .setSmallIcon(R.drawable.ic_main_logo)
+                .setContentTitle(context.getString(R.string.app_name))
+                .setContentText(context.getString(R.string.notification));
+        mBuilder.setContentIntent(pi);
+        mBuilder.setDefaults(Notification.DEFAULT_SOUND);
+        mBuilder.setAutoCancel(true);
+        NotificationManagerCompat mNotificationManager = NotificationManagerCompat.from(context);
+        mNotificationManager.notify(200, mBuilder.build());
+    }
+}
--- app/src/main/java/com/example/budgetup/SplashActivity.java
+++ app/src/main/java/com/example/budgetup/SplashActivity.java
@@ -3,9 +3,14 @@ package com.example.budgetup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 
 import java.util.List;
@@ -14,11 +19,12 @@ import java.util.Locale;
 @SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
 
+  int REQUEST_CODE = 0;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_splash);
-
+    createNotificationChannel();
     AppDatabase db = AppDatabase.build(getApplicationContext());
     List<User> users = db.userDao().getAll();
     if (users.size() != 0) {
@@ -28,10 +34,28 @@ public class SplashActivity extends AppCompatActivity {
       Configuration configuration = resources.getConfiguration();
       configuration.setLocale(locale);
       resources.updateConfiguration(configuration, resources.getDisplayMetrics());
+
+      Intent intent = new Intent(SplashActivity.this, Receiver.class);
+      PendingIntent pendingIntent = PendingIntent.getBroadcast(SplashActivity.this, REQUEST_CODE, intent, 0);
+      AlarmManager am = (AlarmManager)getSystemService(ALARM_SERVICE);
+      am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), AlarmManager.INTERVAL_DAY *7, pendingIntent);
       startActivity(new Intent(SplashActivity.this, HomeActivity.class));
 
     } else {
       startActivity(new Intent(SplashActivity.this, MainActivity.class));
+    }
+  }
+
+  private void createNotificationChannel() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      CharSequence name = "BudgetUpChannel";
+      String description = "Channel for BudgetUp";
+      int importance = NotificationManager.IMPORTANCE_DEFAULT;
+      NotificationChannel channel = new NotificationChannel("notifyBudgetUp", name, importance);
+      channel.setDescription(description);
+
+      NotificationManager notificationManager = getSystemService(NotificationManager.class);
+      notificationManager.createNotificationChannel(channel);
     }
   }
 }
